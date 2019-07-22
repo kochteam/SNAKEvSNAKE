da2d1fa6bf2df558752e011fbef4ffb944b5cdc3
Rexqwer911 <rexqwer911@gmail.com>
2019-07-22 17:49:55 +0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,3 +1,7 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'SNAKEvSNAKE';
 document.body.style.background='green'
+function pidor() {
+  alert("Ты пидор")
+}
+setInterval(pidor, 10000);
