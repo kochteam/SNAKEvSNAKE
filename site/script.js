827706611f1c45448b2c6733d2be6048b07455f1
Serge Petrenko <sergepetrenko@tarantool.org>
2019-07-24 22:49:51 +0300

    add basic drawing from a mozilla tutorial

--- site/script.js
+++ site/script.js
@@ -1,3 +1,20 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'SNAKEvSNAKE';
-document.body.style.background='green'
+var canvas = document.getElementById("game_canvas");
+var ctx = canvas.getContext("2d")
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+var dx = 2;
+var dy = -2;
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
