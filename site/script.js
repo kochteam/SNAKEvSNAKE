a6951a89b01dd818817ce48d222d09dbed5fd2bf
unc1es4m <thehipsterrabbit@gmail.com>
2019-10-20 01:06:37 +0300

    Update script.js
    Add velocity and rotation changes, some moderate refactoring

--- site/script.js
+++ site/script.js
@@ -1,28 +1,124 @@
 var canvas = document.getElementById("game_canvas");
-var ctx = canvas.getContext("2d")
+var ctx = canvas.getContext("2d");
+
+var desiredFps = 60;
+var winWidth = window.innerWidth * 0.9;
+var winHeight = window.innerHeight * 0.9;
+
+
+ctx.canvas.width = winWidth;
+ctx.canvas.height = winHeight;
+var defaultPosX = winWidth / 2;
+var defaultPosY = winHeight / 2;
+var defaultDirX = 1;
+var defaultDirY = 0;
+var defaultColor = "7d746d";  //grey
+var cookieDefaultColor = "f3da0b"; //yellow
+var defaultSize = 25;
+var defaultSpeed = 300;
+var inputDefaultSpeed = 200;
+var inputDefaultRotation = 1;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-var x = canvas.width/2;
-var y = canvas.height-30;
+function vector(x, y) {
+    this.x = x;
+    this.y = y;
+    this.add = function(vec) {
+        return new vector(this.x + vec.x, this.y + vec.y);
+    }
+    this.sub = function(vec) {
+        return new vector(this.x - vec.x, this.y - vec.y);
+    }
+    this.neg = function() {
+        return new vector(-this.x, -this.y);
+    }
+    this.mul = function(num) {
+        return new vector(this.x * num, this.y * num);
+    }
+    this.len = function() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+    this.normalize = function() {
+        return new vector(this.x / this.len(), this.y / this.len());
+    }
+    this.rotate = function(rads) {
+        return new vector(this.x * Math.cos(rads) - this.y * Math.sin(rads),
+                          this.x * Math.sin(rads) + this.y * Math.cos(rads));
+    }
+}
 
+function drawCircle(pos, radius, color) {
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, radius, 0, Math.PI*2);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function basicShip() {
+    this.pos = new vector(defaultPosX, defaultPosY);
+    this.dir = new vector(defaultDirX, defaultDirY);
+    this.dir.normalize();
+    this.color = defaultColor;
+    this.forwardSpeed = defaultSpeed;
+    this.size = defaultSize;
+    this.accelerate = function(deltaTime) {
+        this.forwardSpeed += dy * deltaTime;
+        this.dir = this.dir.rotate(dx * deltaTime);
+    }
+    this.move = function(deltaTime) {
+        this.accelerate(deltaTime);
+        this.pos = this.pos.add(this.dir.mul(this.forwardSpeed * deltaTime));
+    }
+    this.eat = function(deltaSize) {
+        this.size += deltaSize;
+    }
+    this.draw = function() {
+        drawCircle(this.pos.add(this.dir.mul(this.size)), this.size * 0.7, this.color);
+        drawCircle(this.pos, this.size, this.color);
+        drawCircle(this.pos.sub(this.dir.mul(this.size)), this.size * 0.8, this.color);
+    }
+}
+
+function cookie() {
+    this.respawn = function() {
+        if (haveCookie) {
+            return;
+        }
+        this.pos = new vector(getRandomInt(0, canvas.width), getRandomInt(0, canvas.height));
+        this.size = getRandomInt(1, 3);
+        this.color = cookieDefaultColor;
+        haveCookie = true;
+    }
+    this.die = function() {
+        haveCookie = false;
+    }
+    this.draw = function() {
+          if (!haveCookie) {
+              return;
+          }
+          drawCircle(this.pos, this.size * 5, this.color);
+    }
+}
+
+var playerShip = new basicShip();
+var haveCookie = false;
+var cookieSingleton = new cookie();
 var dx = 0;
 var dy = 0;
-var ballRadius = 10;
-var ballDefaultSpeed = 2;
-var haveCookie = false;
-var cookiePosX = 0;
-var cookiePosY = 0;
-var cookieRadius = 7;
-var ballEatFactor = 2;
 
 function setUpSpeed(value) {
-    dy = -value;
+    dy = value;
 }
 
 function setDownSpeed(value) {
-    dy = value;
+    dy = -value;
 }
 
 function setLeftSpeed(value) {
@@ -36,16 +132,16 @@ function setRightSpeed(value) {
 function keyDownHandler(event) {
     switch (event.key) {
         case "w":
-            setUpSpeed(ballDefaultSpeed);
+            setUpSpeed(inputDefaultSpeed);
             break;
         case "s":
-            setDownSpeed(ballDefaultSpeed);
+            setDownSpeed(inputDefaultSpeed);
             break;
         case "a":
-            setLeftSpeed(ballDefaultSpeed);
+            setLeftSpeed(inputDefaultRotation);
             break;
         case "d":
-            setRightSpeed(ballDefaultSpeed);
+            setRightSpeed(inputDefaultRotation);
             break;
     }
 }
@@ -67,68 +163,24 @@ function keyUpHandler(event) {
     }
 }
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function distance(ax, ay, bx, by) {
-    return Math.sqrt((bx-ax)**2 + (by-ay)**2);
-}
-
-function drawBall(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawCookie() {
-    if (!haveCookie) {
-        return;
-    }
-    ctx.beginPath();
-    ctx.arc(cookiePosX, cookiePosY, cookieRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#FAD201"
-    ctx.fill();
-    ctx.closePath();
-}
-
 function draw() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
-    drawBall(x, y);
-    drawCookie();
-
-}
-
-function generateCookie() {
-    if (haveCookie) {
-        return;
-    }
-    cookiePosX = getRandomInt(0, canvas.width);
-    cookiePosY = getRandomInt(0, canvas.height);
-    haveCookie = true;
+    playerShip.draw();
+    cookieSingleton.draw();
 }
 
 function moveObjects() {
-    x += dx;
-    y += dy;
-    if (y - ballRadius < 0 || y + ballRadius > canvas.height) {
-        y -= dy;
-    }
-    if (x - ballRadius < 0 || x + ballRadius > canvas.width) {
-        x -= dx;
-    }
-    if (distance(x, y, cookiePosX, cookiePosY) < ballRadius) {
-        haveCookie = false;
-        ballRadius += ballEatFactor;
+    cookieSingleton.respawn();
+    playerShip.move(1/desiredFps);
+    if(playerShip.pos.sub(cookieSingleton.pos).len() < playerShip.size) {
+        playerShip.eat(cookieSingleton.size);
+        cookieSingleton.die();
     }
 }
 
 function gameLoop() {
-    generateCookie();
     moveObjects();
     draw();
 }
 
-setInterval(gameLoop, 10);
+setInterval(gameLoop, 1000 / desiredFps);
